6752d05092568a46c85ed29eca838a55145f42f3
esma <esma.ea.aksoy@gmail.com>
2023-11-23 22:29:17 +0300

    end

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.scss";
-const searchInput = document.querySelector("#searchInput")
 
-searchInput.addEventListener("input",()=>{
-  getProducts()
-})
 
 const getProducts = async()=>{
-
 try {
-  const res = await fetch("am/api/products/")
-  console.log(res);
+  const res = await fetch("https://anthonyfs.pythonanywhere.com/api/products/")
  if(!res.ok){
   throw new Error(`Error: ${res.status}`)
  }
  const data = await res.json()
-console.log(data);
-// showData(data)
+showData(data)
 } catch (error) {
   console.log(error);
 }
 }
-
 getProducts()
 
 const showData=(product)=>{
   product.forEach(item=>{
-    const {title} = item
-   
+    const {title,category,description,image,price} = item
+   const products= document.querySelector("#products")
+   products.innerHTML += `
+    <div class="col">
+          <div class="card">
+            <img
+              src="${image}"
+              class="p-2"
+              height="250px"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title line-clamp-1">Ürün Adı</h5>
+              <p class="card-text line-clamp-3">açıklama</p>
+            </div>
+            <div
+              class="card-footer w-100 fw-bold d-flex justify-content-between gap-3"
+            >
+              <span>Price:</span><span>Fiyat $</span>
+            </div>
+            <div class="card-footer w-100 d-flex justify-content-center gap-3">
+              <button class="btn btn-danger">Sepete Ekle</button>
+              <button
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                See Details
+              </button>
+            </div>
+          </div>
+        </div> 
+   `
   })
 
-
 }
+
+window.addEventListener("load", ()=>{
+  getProducts()
+})
