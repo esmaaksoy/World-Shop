89fb93c2a094d1e9d2e6dd5bcd79f4fb69f0adde
esma <esma.ea.aksoy@gmail.com>
2023-11-23 14:33:37 +0300

    first

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.scss";
+const searchInput = document.querySelector("#searchInput")
+
+searchInput.addEventListener("input",()=>{
+  getProducts()
+})
+
+const getProducts = async()=>{
+
+try {
+  const res = await fetch("https://anthonyfs.pythonanywhere.com/api/products/")
+ if(!res.ok){
+  throw new Error(`Error: ${res.status}`)
+ }
+ const data = await res.json()
+showData(data)
+} catch (error) {
+  console.log(error);
+}
+}
+
+getProducts()
+
+const showData=(product)=>{
+  product.forEach(item=>{
+    const {title} = item
+    console.log(title);
+  })
+
+
+}
