d59341852005a3b8e5d0310df91a0f3c146749c3
esma <esma.ea.aksoy@gmail.com>
2023-11-25 18:01:23 +0300

    style

--- selectedCategory.js
+++ selectedCategory.js
@@ -1,6 +1,5 @@
 export const selectedCategory= (categories)=>{
   const categoryArr = ["All", ...new Set(categories.map(item=> item.category))]
-console.log(categoryArr);
     // const uniqueCategories= categories.reduce( (accumulator, product) =>{
     //   if (!accumulator.includes(product.category)) {
     //     accumulator.push(product.category);
@@ -8,10 +7,9 @@ console.log(categoryArr);
     //   return accumulator;
     // }, ["All"]);
     const butons= document.querySelector("#btns")
-    // const btnColors=["primary","secondary","success","danger","warning","info"]
 categoryArr.forEach((category, index)=>{
   const btn = document.createElement("button")
-  btn.innerText= category.toUpperCase();
+  btn.innerText= category;
   btn.classList.add("btn",'btn-outline-primary')
   butons.appendChild(btn)
 })
